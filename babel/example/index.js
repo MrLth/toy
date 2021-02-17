6cf64e3f286affc03accccbc7ff1ed52598e3ae5
mrlth <mrlth@qq.com>
2021-02-17 15:58:50 +0800

    upd babel ployfill and transform-to-runtime

---
+++ babel/example/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: mrlthf11
+ * @LastEditors: mrlthf11
+ * @Date: 2021-02-17 15:16:28
+ * @LastEditTime: 2021-02-17 15:55:24
+ * @Description: file content
+ */
+
+for (const v of [1, 2, 3]) {
+  console.log(v)
+}
+
+[1,2,3].includes(3)
+
+new Map()
+
+[1].flat()
