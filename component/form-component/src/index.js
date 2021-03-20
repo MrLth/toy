f9fe346dca95714ec173422ea302b2da7fe31e98
mrlthf11 <mrlth@qq.com>
2021-03-20 18:53:04 +0800

    update dynamic-form

--- component/form-component/src/index.js
+++ component/form-component/src/index.js
@@ -2,17 +2,18 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2021-02-01 23:22:28
- * @LastEditTime: 2021-02-02 10:09:55
+ * @LastEditTime: 2021-03-20 18:13:51
  * @Description: file content
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import Form from './Form'
-import FormHook from './FormHook'
+// import FormHook from './FormHook'
+import DynamicForm from './DynamicForm'
 
 ReactDOM.render(
-  <FormHook />,
+  <DynamicForm />,
   document.getElementById('root')
 );
 
